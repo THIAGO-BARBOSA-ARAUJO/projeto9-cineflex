8c3446b199f30318f7996ddd53b985d757068bf2
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-07-27 00:57:19 -0300

    inicio do projeto

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "../src/components/App"
+import "../src/css/reset.css"
+import "../src/css/style.css"
+
+
+ReactDOM.render(<App/>, document.querySelector('.root'))
