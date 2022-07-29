3756274ae7843de7bb7364486ee447ce76c3c41d
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-07-29 00:21:33 -0300

    implementei a funcionalidade de pegar os acentos da sessão

--- src/components/sessao/Sessaofilme.js
+++ src/components/sessao/Sessaofilme.js
@@ -1,16 +1,36 @@
 import { useParams } from "react-router-dom"
+import { useState, useEffect } from 'react';
+import "./sessaofilme.css"
+import axios from 'axios';
 
-export default function(){
+export default function Sessaofilme(){
 
     const {idsessao} = useParams()
+    const [assentos, setAssentos] = useState([])
+
+	useEffect(() => {
+		const requisicao = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/showtimes/${idsessao}/seats`);
+
+		requisicao.then((resposta) => {
+            setAssentos(resposta.data.seats)
+            console.log(resposta.data.seats)
+		})
+	}, [])
 
     return(
         <div className="sessao">
             <h1>Selecione o(s) assento(s)</h1>
             <div className="asentos">
-                <div className="assento">
-                    <p></p>
-                </div>
+                {assentos.map((assento)=>{
+                    return(
+                        <div className="assento">
+                            <p>{assento.name}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className="indicacoes">
+                
             </div>
         </div>
     )
